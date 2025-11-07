1ca3fff62a4d4819cc73f9bf57f2759007757b59
sumanth338 <sumanthashwin000@gmail.com>
2025-11-07 15:32:33 +0530

    logot feature is implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { removeUser } from '../utils/userSlice'
+import { useDispatch } from 'react-redux'
 const Navbar = () => {
   const user = useSelector((store) => store.user)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+    const handleLogout = async ()=>{
+      try{
+        await axios.post("http://localhost:3000/logout",{}, {withCredentials:true})
+        dispatch(removeUser())
+        return navigate('/login')
+      }
+      catch(err){
+        console.log(err)
+      }
+    }
   return (
     <div className="navbar bg-base-200 shadow-sm">
   <div className="flex-1">
@@ -31,7 +46,7 @@ const Navbar = () => {
           </Link>
         </li>
         <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
+        <li><a onClick={handleLogout}>Logout</a></li>
       </ul>
     </div>
   </div>
